d46fcd8da7687aeb552e49fab7fe0f3f8ab2f01e
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2020-08-05 10:39:38 +0200

    Time complexity O(n^2)

---
+++ addAndSearch.java
@@ -0,0 +1,50 @@
+class WordDictionary {
+    
+    List<String> books = new ArrayList<String>();
+    
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+       
+        
+    }
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+       books.add(word);
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        
+        if(word.length()==0)return false;
+        char[] tab = word.toCharArray();
+        char[] tab2;
+        
+        for (String curr : books) {
+                int found = 1;
+                tab2=curr.toCharArray();
+                if(tab.length!=tab2.length)continue;
+               //System.out.println("Current word: "+word);
+                //System.out.println("Comparing:"+curr);
+                for(int i = 0; i<tab.length; i++){
+                   if((word.charAt(i)=='.' || tab[i]==tab2[i]) && found==1){
+                            //System.out.println("THHERE IS A DOT"+word.charAt(i));
+                            found=1;
+                    }else{
+                            found=0;
+                            break;
+                    }
+                    if(i==(tab.length-1) && found==1)return true;
+                }
+            
+        }
+        return false;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
