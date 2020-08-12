1099ba662a9bc9a06b14784faaca70d318e6e4a2
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2020-08-12 10:28:59 +0200

    Time complexity O(log(n))

---
+++ searchBinarySearchTree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode result = search(root,val);
+        return result;
+    }
+    public TreeNode search(TreeNode node, int target){
+         if(node==null)return null;
+         if(node.val==target)return node;
+        
+         if(node.val<target){
+            return search(node.right,target);
+         }else return search(node.left,target);
+    }
+}
