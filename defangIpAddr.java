cec3df0b282eb7cc0e0ddd0cdda44cf2f89917d1
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2020-08-22 17:31:04 +0200

    Time complexity O(n)

---
+++ defangIpAddr.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        String res="";
+        
+        
+        for(int i = 0; i<address.length();i++){
+            if(address.charAt(i)=='.'){
+                res=res+"[.]";
+            }else{
+                res=res+address.charAt(i);
+            }
+        }
+        
+        return res;
+    }
+}
