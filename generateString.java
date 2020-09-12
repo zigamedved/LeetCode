7f1e75ce3b73fe0e97718636da502cf9a85c20c2
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2020-09-12 17:43:08 +0200

    Time complexity O(n)

---
+++ generateString.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String generateTheString(int n) {
+        String res="";
+        if(n%2==0){
+            for(int i = 0; i<n-1; i++){
+                res+='a';
+            }
+            res+='b';
+            return res;
+        }else{
+            for(int i = 0; i<n; i++){
+                res+='a';
+            }
+           return res; 
+        }
+        
+    }
+}
