41ad4401a1760d2006c460824d22bdf860492bf0
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2020-09-25 19:20:39 +0200

    Update addAndSearch.java

--- addAndSearch.java
+++ addAndSearch.java
@@ -41,10 +41,3 @@ class WordDictionary {
         return false;
     }
 }
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary obj = new WordDictionary();
- * obj.addWord(word);
- * boolean param_2 = obj.search(word);
- */
