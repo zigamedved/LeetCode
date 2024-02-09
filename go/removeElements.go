58ceac1853ee10ca126ab1344a836a2f2e4f297a
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2024-02-09 20:16:41 +0100

    add removeElements solution

---
+++ go/removeElements.go
@@ -0,0 +1,12 @@
+func removeElement(nums []int, val int) int {
+	insertIdx := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[insertIdx] = nums[i]
+			insertIdx++
+		}
+	}
+
+	return insertIdx
+}
